eabdd2eafd3690ef136f853d8162313159c2bd72
craig-mclean <107093540+craig-mclean@users.noreply.github.com>
2022-09-22 20:12:58 +1200

    comment out router for now, to help pass linter

--- server/routes/playlist.js
+++ server/routes/playlist.js
@@ -28,34 +28,33 @@ router.get('/id', (req, res) => {
 })
 
 // TO DO adding new playist and tracks at the same time.
-router.post('/', (req, res) => {
-  const data = 'DJSleePete' //req.body.playlistName
-  const tracks = [
-    { track_id: 2 },
-    { track_id: 3 },
-    { track_id: 2 },
-    { track_id: 8 },
-    { track_id: 9 },
-  ]
+// router.post('/', (req, res) => {
+//   const data = 'DJSleePete' //req.body.playlistName
+//   const tracks = [
+//     { track_id: 2 },
+//     { track_id: 3 },
+//     { track_id: 2 },
+//     { track_id: 8 },
+//     { track_id: 9 },
+//   ]
 
-  //let tempId = null
-  db.addPlaylist(data)
-    .then((id) => {
-      //tempId = id
-      tracks.map((x) => {
-        //x.tempId
-        x.id
-      })
-      return db.addTracksToPlaylist(tracks)
-    })
-    .then((data) => {
-      res.json(data)
-      // res.json({ ...captionData, id: ids[0], image_id: tempImageId })
-    })
-    .catch((err) => {
-      console.error(err.message)
-      res.status(500).json({ message: 'Something went wrong' })
-    })
-})
+//   let tempId = null
+//   db.addPlaylist(data)
+//     .then((id) => {
+//       tempId = id
+//       tracks.map((x) => {
+//         x.tempId
+//       })
+//       return db.addTracksToPlaylist(tracks)
+//     })
+//     .then((data) => {
+//       res.json(data)
+//       // res.json({ ...captionData, id: ids[0], image_id: tempImageId })
+//     })
+//     .catch((err) => {
+//       console.error(err.message)
+//       res.status(500).json({ message: 'Something went wrong' })
+//     })
+// })
 
 module.exports = router
