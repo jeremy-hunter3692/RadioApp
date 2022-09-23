d932e6b890fa87e70095b2deaba4186b3aa6769d
peter-pakoti <107296263+peter-pakoti@users.noreply.github.com>
2022-09-23 14:20:29 +1200

    getTracksById passing, unsure about toHaveLength

--- server/db/__tests__/tracks.test.js
+++ server/db/__tests__/tracks.test.js
@@ -20,7 +20,6 @@ describe('getAllTracks', () => {
   test('gets all the tracks from the database', () => {
     expect.assertions(7)
     return getAllTracks(testDb).then((tracks) => {
-      console.log(tracks)
       expect(tracks[8].id).toBe(9)
       expect(tracks[8].filepath).toBe('../public/tracks9')
       expect(tracks[8].title).toBe('handle this')
@@ -28,6 +27,23 @@ describe('getAllTracks', () => {
       expect(tracks[8].album).toBe('Can you handle it')
       expect(tracks[8].notes).toBe("it's lovely")
       expect(tracks).toHaveLength(9)
+    })
+  })
+})
+
+describe('getTracksById', () => {
+  test('gets single track by id', () => {
+    const trackId = '1'
+    expect.assertions(6)
+    return getTrackById(trackId, testDb).then((track) => {
+      console.log(track)
+      expect(track.id).toBe(1)
+      expect(track.filepath).toBe('../public/tracks1')
+      expect(track.title).toBe('jam1')
+      expect(track.artist).toBe('DJ wildcard')
+      expect(track.album).toBe('Watch yo back')
+      expect(track.notes).toBe('The best album in the world')
+      // expect(track).toHaveLength(1) Want to show that we are getting one track back
     })
   })
 })
