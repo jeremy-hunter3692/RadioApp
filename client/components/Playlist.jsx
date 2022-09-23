8cac3a668b975c24127a58476f5d176266b11daa
Haunani Pao <90392354+haunanipao@users.noreply.github.com>
2022-09-23 15:13:21 +1200

    We got data. Thank you Jeremy and David

--- client/components/Playlist.jsx
+++ client/components/Playlist.jsx
@@ -1,37 +1,12 @@
 import React from 'react'
-const info = [
-  {
-    id: 1,
-    name: 'The Fillmore',
-    creator: 'DJ Craig',
-    imageFilepath: './images/mixtape1.png',
-    // note - just make a random generator of images and put it with the playlist component.
-  },
-  {
-    id: 2,
-    name: 'K-Pop',
-    creator: 'Nani',
-    imageFilepath: './images/mixtape16.png',
-  },
-  {
-    id: 3,
-    name: 'The Boom Boom Room',
-    creator: 'DJ Jeremy',
-    imageFilepath: './images/mixtape3.png',
-  },
-  {
-    id: 4,
-    name: 'Rolling Stone',
-    creator: 'DJ Peter',
-    imageFilepath: './images/mixtape8.png',
-  },
-]
-// Don't forget: when db is available to pass the prop {dbName}
+import { useSelector } from 'react-redux'
+
 export default function Playlist() {
-  console.log(info)
+  const info = useSelector((state) => state.playlist?.data)
+  console.log('playlist component info', info)
   return (
     <>
-      {info.map((playlist) => {
+      {info?.map((playlist) => {
         return (
           <div to={'/'} key={playlist.id} className='playlist'>
             <img
@@ -39,8 +14,8 @@ export default function Playlist() {
               alt={playlist.name}
               width='100px'
             />
-            <h3>{playlist.name}</h3>
-            <h3>{playlist.creator}</h3>
+            <h3>{playlist.title}</h3>
+            <h3>{playlist.artist}</h3>
           </div>
         )
       })}
