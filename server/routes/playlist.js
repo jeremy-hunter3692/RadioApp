2dfb2c09bddba2c351069dcee4b2468ef0ccd50c
peter-pakoti <107296263+peter-pakoti@users.noreply.github.com>
2022-09-26 09:46:27 +1300

    merging after lint checks

--- server/routes/playlist.js
+++ server/routes/playlist.js
@@ -42,7 +42,7 @@ router.post('/', (req, res) => {
   console.log(req)
   db.addPlaylist({ name })
     .then((playlist) => {
-      res.json({ id: playlist, name, image })
+      res.json({ id: playlist, name })
       return null
     })
     .catch((err) => {
@@ -63,30 +63,31 @@ router.post('/addTrack', (req, res) => {
       res.status(500).send(err.message)
     })
 })
+
 // TO DO adding new playist and tracks at the same time.
-// router.post('/', (req, res) => {
-//   const data = { name: req.body.playlistName }
-//   console.log('data info', data)
-//   const tracks = req.body.tracks
-//   let tempId = null
-//   db.addPlaylist(data)
-//     .then((id) => {
-//       tempId = id
-//       let result = tracks.map((x) => {
-//         return { ...x, playlist_Id: tempId }
-//       })
-//       return db.addTracksToPlaylist(result)
-//     })
-//     .then((result) => {
-//       res.json(result)
-//       //TO DO look up best practise for sending back json data here
-//       //was in Sarahs lecture
-//       //res.json({ ...captionData, id: ids[0], image_id: tempImageId })
-//     })
-//     .catch((err) => {
-//       console.error(err.message)
-//       res.status(500).json({ message: 'Something went wrong' })
-//     })
-// })
+router.post('/', (req, res) => {
+  const data = { name: req.body.playlistName }
+  console.log('data info', data)
+  const tracks = req.body.tracks
+  let tempId = null
+  db.addPlaylist(data)
+    .then((id) => {
+      tempId = id
+      let result = tracks.map((x) => {
+        return { ...x, playlist_Id: tempId }
+      })
+      return db.addTracksToPlaylist(result)
+    })
+    .then((result) => {
+      res.json(result)
+      //TO DO look up best practise for sending back json data here
+      //was in Sarahs lecture
+      //res.json({ ...captionData, id: ids[0], image_id: tempImageId })
+    })
+    .catch((err) => {
+      console.error(err.message)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
 
 module.exports = router
