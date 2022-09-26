9080bc51b75180384a58f8e3ad537ab3e21fa617
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2022-09-27 08:48:26 +1300

    merged with dev

--- client/apis/playlist.js
+++ client/apis/playlist.js
@@ -23,7 +23,6 @@ export function addPlaylist(newPlaylist) {
     })
     .catch(errorHandler('ADD', rootUrl + `/playlists/`))
 }
-
 // get a Playlist by id
 export function getPlaylistById(id) {
   return request
