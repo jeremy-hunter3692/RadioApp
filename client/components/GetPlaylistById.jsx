2faacdae8925aaf959e6799e011a3e23dc79a37f
Haunani Pao <90392354+haunanipao@users.noreply.github.com>
2022-09-26 18:18:16 +1300

    post merge and clean up. time to move on

--- client/components/GetPlaylistById.jsx
+++ client/components/GetPlaylistById.jsx
@@ -9,9 +9,13 @@ export default function GetPlaylistById() {
   return (
     <>
       <div>
-        <h2>{info && info.length > 0 && info[0].playlistName}</h2>
+        <h2>
+          {info?.tracks &&
+            info?.tracks.length > 0 &&
+            info?.tracks[0].playlistName}
+        </h2>
       </div>
-      {info?.map((playlist) => {
+      {info?.tracks.map((playlist) => {
         return (
           <div key={playlist.trackId}>
             <h3>{playlist.title}</h3>
