007312180ed710fd13086957d2b333271dc7975d
craig-mclean <107093540+craig-mclean@users.noreply.github.com>
2022-09-28 15:39:22 +1300

    sort out field placeholder text

--- client/components/AssignTracks.jsx
+++ client/components/AssignTracks.jsx
@@ -7,6 +7,7 @@ const initialForm = {
   track: '',
   playlist: '',
 }
+// let tempVar = ' '
 
 export default function AssignTracks(props) {
   const [form, setForm] = useState(initialForm)
@@ -31,12 +32,17 @@ export default function AssignTracks(props) {
 
   function handleSubmit(e) {
     e.preventDefault()
-    setAddedTrack(tracks.find((x) => x.id === Number(form.track)))
-    props.bool
-      ? assignTracktoPlaylist(form)
-      : assignTracktoPlaylist({ ...form, playlist: newPlaylistId })
+    // console.log('tempVar', tempVar, 'addedTrack', addedTrack)
+    if (form.track === '') {
+      console.log('NO')
+    } else {
+      setAddedTrack(tracks.find((x) => x.id === Number(form.track)))
+      props.bool
+        ? assignTracktoPlaylist(form)
+        : assignTracktoPlaylist({ ...form, playlist: newPlaylistId })
 
-    setForm(initialForm)
+      setForm(initialForm)
+    }
     // props.nextStep()
   }
 
@@ -54,50 +60,48 @@ export default function AssignTracks(props) {
       {addedTrack?.title != null && <h1>{addedTrack?.title} has been added</h1>}
       <form onSubmit={handleSubmit} className='form'>
         <label htmlFor='track'>Assign a track to a mixtape:</label>
-
-        <select
-          id='track'
-          name='track'
-          defaultValue=''
-          onChange={handleChange}
-          required
-        >
-          <option value='' disabled>
-            Choose track
-          </option>
-          {tracks.map((track) => (
-            <option key={track.id} value={track.id} title='Choose a track'>
-              {track.title}
-            </option>
-          ))}
-        </select>
-
-        {props.bool && (
+        <div>
           <select
-            id='playlist'
-            name='playlist'
+            id='track'
+            name='track'
             defaultValue=''
-            required
             onChange={handleChange}
+            required
           >
             <option value='' disabled>
-              Choose mixtape
+              Choose track
             </option>
-            {playlists.map((playlist) => (
-              <option key={playlist.id} value={playlist.id}>
-                {playlist.name}
+            {tracks.map((track) => (
+              <option key={track.id} value={track.id} title='Choose a track'>
+                {track.title}
               </option>
             ))}
           </select>
-        )}
-        <div>
+
+          {props.bool && (
+            <select
+              id='playlist'
+              name='playlist'
+              defaultValue=''
+              required
+              onChange={handleChange}
+            >
+              <option value='' disabled>
+                Choose mixtape
+              </option>
+              {playlists.map((playlist) => (
+                <option key={playlist.id} value={playlist.id}>
+                  {playlist.name}
+                </option>
+              ))}
+            </select>
+          )}
           <button>Assign track to Mixtape</button>
 
           <button
             onClick={(e) => {
               e.preventDefault()
               setForm(initialForm)
-              props.backStep()
             }}
           >
             Cancel
