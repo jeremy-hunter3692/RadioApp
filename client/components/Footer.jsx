07b9a717b1dab0071f61365e342796929b5e11c3
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2022-10-03 18:31:46 +1300

    last tweaks

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <div>
       <footer>
-        &copy; 2022 / DEV Aotearoa / Manaia / Mixtape / Made by: Jeremy Hunter,
-        Haunani Pao, Craig McLean, Peter Pakoti{' '}
+        &copy; 2022 / DEV Academy Aotearoa / Manaia / Mixtape / Made by: Jeremy
+        Hunter, Haunani Pao, Craig McLean, Peter Pakoti{' '}
       </footer>
     </div>
   )
