93429a597d12a9faf4be8e6774cb12fe557f065a
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2023-07-31 20:28:33 +1200

    drag and drop  sorting woking

--- client/components/DragNDrop.jsx
+++ client/components/DragNDrop.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 
 export default function DnDMenu() {
-  const [menuOrder, setMenuOrder] = useState([])
-  const [oldMenuOrder, setOldMenuOrder] = useState(['one', 'two', 'three'])
+  const [menuOrder, setMenuOrder] = useState(['1', '2', '3', '4', '5'])
+  // const [oldMenuOrder, setOldMenuOrder] = useState(['one', 'two', 'three'])
   const [currentIdx, setCurrentIdx] = useState()
   console.log('top', menuOrder)
 
   function handleOnDrag(e, item) {
     e.dataTransfer.setData('menuItem', item)
-    console.log('drag', item)
   }
 
   function onDragOver(e) {
@@ -16,23 +15,34 @@ export default function DnDMenu() {
   }
 
   function dragEnd() {
-    console.log('end:', currentIdx)
     //use currentIdx to splice array of menuOrder
-    let item = oldMenuOrder[currentIdx]
-    let fixed = oldMenuOrder.splice(currentIdx, 0)
+    // let item = oldMenuOrder[currentIdx]
+    // let fixed = menuOrder.splice(currentIdx, 0, item)
+    // console.log('end:', currentIdx, item, fixed)
   }
 
   function handleDragEnter(e, index) {
     console.log('enter:', index)
     setCurrentIdx(index)
+    e.target.className = 'dropTarget'
+  }
+
+  function handleDragLeave(e, index) {
+    e.target.className = 'menuItem'
   }
 
   function onDrop(e) {
+    e.target.className = 'menuItem'
     const item = e.dataTransfer.getData('menuItem')
-    setMenuOrder([...menuOrder, item])
-    const filter = oldMenuOrder.filter((x) => x != item)
+    console.log('Drop item:', item)
+    //remove old thing first
+    let temp = menuOrder.filter((x) => x != item)
+    temp.splice(currentIdx, 0, item)
+    console.log('fixed', temp)
+    setMenuOrder(temp)
+    // const filter = oldMenuOrder.filter((x) => x != item)
     // console.log('filtered', filter)
-    setOldMenuOrder(filter)
+    // setOldMenuOrder(filter)
   }
 
   function menuItemCreator(name) {
@@ -50,16 +60,18 @@ export default function DnDMenu() {
 
   return (
     <>
-      {oldMenuOrder.map((x) => {
+      {/* {oldMenuOrder.map((x) => {
         return menuItemCreator(x)
-      })}
+      })} */}
       <div className='menuHolder' onDrop={onDrop} onDragOver={onDragOver}>
         {menuOrder.map((item, index) => {
           return (
             <div
+              className='menuItem'
               draggable
               onDragStart={(e) => handleOnDrag(e, item)}
               onDragEnter={(e) => handleDragEnter(e, index)}
+              onDragLeave={(e) => handleDragLeave(e, index)}
               onDragEnd={dragEnd}
               key={index}
             >
